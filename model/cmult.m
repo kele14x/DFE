961460c1661b9392ff6fee19472d9d2ac5ca84ac
Michiru Morishita <kokser.merio@gmail.com>
2020-12-21 01:05:54 +0800

    add test bench for cmult

--- model/cmult.m
+++ model/cmult.m
@@ -1,8 +1,8 @@
-function p = cmult(a, b, varargin)
+function [p, ovf] = cmult(a, b, varargin)
 % CMULT performs complex multiplier with the way of hareware implemented
 %
-%   p = cmult(a, b)
-%   p = cmult(a, b, Name, Value)
+%   [p, ovf] = cmult(a, b)
+%   [p, ovf] = cmult(a, b, Name, Value)
 %
 % Input Arguments:
 %
@@ -20,35 +20,55 @@ function p = cmult(a, b, varargin)
 %
 %     `p` is complex multiply product of `a` and `b`
 %
+%     `ovf` is overflow indicator
+%
 % See also PROD.
 
 % Copyright 2020 kele14x
 
 %% Parse Arguments
-p = inputParser;
+pa = inputParser;
 
-addParameter(p, 'RightShiftBits', 0, @(x)(isscalar(x) && isnumeric(x)));
-addParameter(p, 'RoundMode', 'None', ...
+addParameter(pa, 'AWordLength', 16, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'AFractionLength', 15, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'BWordLength', 16, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'BFractionLength', 15, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'PWordLength', 16, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'PFractionLength', 15, @(x)(isscalar(x) && isnumeric(x)));
+addParameter(pa, 'RoundMode', 'None', ...
     @(x)(ismember(x, {'Truncate', 'PositiveInfinity', 'None'})));
 
-parse(p, varargin{:});
+parse(pa, varargin{:});
 
 %% 3-Multiply Structure
-s1 = (real(a) - imag(a)) * imag(b);
-s2 = (real(b) - imag(b)) * real(a);
-s3 = (real(b) + imag(b)) * imag(a);
+s1 = (real(a) - imag(a)) .* imag(b);
+s2 = (real(b) - imag(b)) .* real(a);
+s3 = (real(b) + imag(b)) .* imag(a);
 
-pr = (s1 + s2) / 2^p.Results.RightShiftBits;
-pi = (s1 + s3) / 2^p.Results.RightShiftBits;
+pr = (s1 + s2);
+pi = (s1 + s3);
 
-if strcmp(p.Results.RoundMode, 'Truncate')
+%% Output
+
+sra = pa.Results.AFractionLength + pa.Results.BFractionLength - pa.Results.PFractionLength;
+RND = 0;
+if sra > 0 && strcmp(pa.Results.RoundMode, 'PositiveInfinity')
+    RND = 2^(sra - 1);
+end
+
+pr = (pr + RND) / 2^sra;
+pi = (pi + RND) / 2^sra;
+
+if strcmp(pa.Results.RoundMode, 'Truncate') || ...
+        strcmp(pa.Results.RoundMode, 'PositiveInfinity')
     pr = floor(pr);
     pi = floor(pi);
-elseif strcmp(p.Results.RoundMode, 'PositiveInfinity')
-    pr = floor(pr+0.5);
-    pi = floor(pi+0.5);
 end
 
 p = complex(pr, pi);
+ovf = pr <= -2^(pa.Results.PWordLength - 1) - 1;
+ovf = ovf | pr >= 2^(pa.Results.PWordLength - 1);
+ovf = ovf | pi <= -2^(pa.Results.PWordLength - 1) - 1;
+ovf = ovf | pi >= 2^(pa.Results.PWordLength - 1);
 
 end
---
+++ test/test_cmult.m
@@ -0,0 +1,72 @@
+%%
+% Copyright (C) 2020 kele14x
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+%% Clear
+clc;
+clearvars;
+close all;
+
+%% Parameters
+AWordLength = 16;
+AFractionLength = 15;
+BWordLength = 16;
+BFractionLength = 15;
+PWordLength = 16;
+PFractionLength = 15;
+RoundMode = 'PositiveInfinity';
+
+sz = [4096, 1];
+
+%% Generate Test Vector
+rng(12345);
+rgA = [-2^(AWordLength - 1), 2^(AWordLength - 1) - 1];
+rgB = [-2^(BWordLength - 1), 2^(BWordLength - 1) - 1];
+
+a = complex(randi(rgA, sz), randi(rgA, sz));
+b = complex(randi(rgB, sz), randi(rgB, sz));
+
+p_ref = a .* b;
+p_ref = p_ref / 2^(AFractionLength + BFractionLength - PFractionLength);
+if strcmp(RoundMode, 'Truncate')
+    p_ref = floor(p_ref);
+elseif strcmp(RoundMode, 'PositiveInfinity')
+    p_ref = floor(p_ref+0.5+0.5j);
+end
+
+ovf_ref = (real(p_ref) <= -2^(PWordLength - 1) - 1) | ...
+    (real(p_ref) >= 2^(PWordLength - 1)) | ...
+    (imag(p_ref) <= -2^(PWordLength - 1) - 1) | ...
+    (imag(p_ref) >= 2^(PWordLength - 1));
+
+%% Test
+[p, ovf] = cmult(a, b, 'AWordLength', AWordLength, ...
+    'AFractionLength', AFractionLength, ...
+    'BWordLength', BWordLength, ...
+    'BFractionLength', BFractionLength, ...
+    'RoundMode', RoundMode);
+
+%% Analysis Result
+assert(all(p == p_ref));
+assert(all(ovf == ovf_ref));
+
+%% Write Text File
+writehex(real(a), fullfile(dfepath(), './data/test_cmult_input_a_real.txt'), AWordLength);
+writehex(imag(a), fullfile(dfepath(), './data/test_cmult_input_a_imag.txt'), AWordLength);
+writehex(real(b), fullfile(dfepath(), './data/test_cmult_input_b_real.txt'), BWordLength);
+writehex(imag(b), fullfile(dfepath(), './data/test_cmult_input_b_imag.txt'), BWordLength);
+writehex(real(p), fullfile(dfepath(), './data/test_cmult_output_p_real.txt'), PWordLength);
+writehex(imag(p), fullfile(dfepath(), './data/test_cmult_output_p_imag.txt'), PWordLength);
+writehex(ovf, fullfile(dfepath(), './data/test_cmult_output_ovf.txt'), 1);
