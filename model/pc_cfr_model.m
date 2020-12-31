fd708b7b285fef5b1fd389f76b964874f91f9ee4
Michiru Morishita <kokser.merio@gmail.com>
2020-12-31 17:20:03 +0800

    cfr_softclipping: renamed to pc_cfr

---
+++ model/pc_cfr_model.m
@@ -0,0 +1,102 @@
+function y = pc_cfr_model(x, varargin)
+% CFR_SOFTCLIPPING performs performs PC-CFR on input signal
+%
+%   y = cfr_softclipping(x, threshold, cPulse)
+%   y = cfr_softclipping(x, threshold, cPulse, Name, Value)
+%
+% Input Arguments:
+%
+%   `x` is input complex waveform
+%
+%   `threhold` is peak detection and clipping threshold
+%
+%   `cPulse` is cancellation pulse of Cancellation Pulse Generators (CPG)
+%
+%   `Name` & 'Value` are Name-Value pairs to hold optional configurations
+%   for this model. Valid arguments are:
+%
+%     `RightShiftBits`: Scalar integer, arithmetic right shift bits on
+%     output
+%
+%     `RoundMode`: 'Truncate', 'PositiveInfinity' or 'None'
+%
+%  Output Arguments:
+%
+%     `p` is complex multiply product of `a` and `b`
+%
+%     `ovf` is overflow indicator
+%
+% See also CFR_HARDCLIPPING.
+
+% Copyright 2020 kele14x
+
+%% Parse Arguments
+p = inputParser;
+
+addParameter(p, 'HB1', [], @(x)(isnumeric(x) && isvector(x)));
+addParameter(p, 'Threshold', [], @(x)(isnumeric(x) && isscalar(x)));
+addParameter(p, 'CancellationPulse', [], @(x)(isnumeric(x) && isvector(x)));
+addParameter(p, 'RoundMode', 'None', @(x)(ismember(x, {'Truncate', 'None'})));
+
+parse(p, varargin{:});
+
+%% Reshape Cancellation Pulse
+cPulse = p.Results.CancellationPulse;
+delay = (length(cPulse) - 1) / 4;
+cPulse = [0, cPulse];
+
+cPulse1 = cPulse(2:2:end);
+cPulse2 = cPulse(1:2:end);
+
+%% Data Path
+
+% Halfband UP2
+hb1 = p.Results.HB1;
+x2 = hb_up2(x, hb1, ...
+    'XinWordLength', 16, ...
+    'XinFractionLength', 15, ...
+    'CoeWordLength', 16, ...
+    'CoeFractionLength', 15, ...
+    'YoutWordLength', 16, ...
+    'YoutFractionLength', 15, ...
+    'CompensateDelay', true, ...
+    'CompensatePower', false, ...
+    'RoundMode', 'PositiveInfinity');
+
+% CORDIC cart2pol
+[x2_thetab, x2_r] = cordic_translate(real(x2), imag(x2), ...
+    'Iterations', 7, ...
+    'CompensationScaling', 'AddSub', ...
+    'PhaseFormat', 'Binary', ...
+    'RoundMode', 'None');
+
+% Peak Detector
+threshold = p.Results.Threshold;
+
+% Peak is defined as sample exceed threshold and larger than neighbors
+is_peak = x2_r > threshold;
+is_peak = is_peak & (x2_r > circshift(x2_r, 1));
+is_peak = is_peak & (x2_r > circshift(x2_r, -1));
+
+% Get the peak value exceed threshold
+peak = x2_r - threshold;
+peak(~is_peak) = 0;
+[peaki, peakq] = cordic_rotate(peak, 0, x2_thetab, ...
+    'Iterations', 7, ...
+    'CompensationScaling', 'AddSub', ...
+    'PhaseFormat', 'Binary', ...
+    'RoundMode', 'None');
+peak = complex(peaki, peakq);
+
+peak = reshape(peak, 2, []).';
+
+delta = zeros(size(peak));
+delta(:, 1) = cconv(cPulse1, peak(:, 1), length(peak(:, 1))) / 2^15;
+delta(:, 2) = cconv(cPulse2, peak(:, 2), length(peak(:, 2))) / 2^15;
+delta = circshift(delta, -delay);
+
+y = x;
+y = y - delta(:, 1);
+y = y - delta(:, 2);
+
+end
